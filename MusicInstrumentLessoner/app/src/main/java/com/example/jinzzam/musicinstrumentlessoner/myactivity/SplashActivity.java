5a2492830f899958eaf18c02e24b4b85f469d4d9
jinzzam <firstmind_@naver.com>
2018-08-18 01:32:30 +0900

    refactoring progressing
    # MainActivity.java

--- MusicInstrumentLessoner/app/src/main/java/com/example/jinzzam/musicinstrumentlessoner/myactivity/SplashActivity.java
+++ MusicInstrumentLessoner/app/src/main/java/com/example/jinzzam/musicinstrumentlessoner/myactivity/SplashActivity.java
@@ -20,7 +20,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.act_splash);
 
         new Thread(() -> {
             try {
