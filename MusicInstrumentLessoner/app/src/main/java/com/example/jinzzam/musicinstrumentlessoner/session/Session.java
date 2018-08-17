8ad3d0f20f9f076603c1101aab08d049afbbdeaf
jinzzam <firstmind_@naver.com>
2018-08-18 03:34:56 +0900

    progressing work for get session
    # Session.java

--- MusicInstrumentLessoner/app/src/main/java/com/example/jinzzam/musicinstrumentlessoner/session/Session.java
+++ MusicInstrumentLessoner/app/src/main/java/com/example/jinzzam/musicinstrumentlessoner/session/Session.java
@@ -1,9 +1,37 @@
 package com.example.jinzzam.musicinstrumentlessoner.session;
 
-public class Session {
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.jinzzam.musicinstrumentlessoner.model.dto.UserDto;
+import com.example.jinzzam.musicinstrumentlessoner.myactivity.LoginActivity;
+
+public class Session extends LoginActivity {
     private static Session instance;
 
+    private static final String TAG = "SESSION";
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etUsername;
+    private Button btnLogin;
+    private RequestQueue queue;
+    private StringRequest stringRequest;
+
+    private UserDto user;
+
     private Session() {
+
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        queue = Volley.newRequestQueue(this);
 
     }
 
