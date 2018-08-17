19ead51368f10fd952ad61fe14ce2fe7408a73a1
jinzzam <firstmind_@naver.com>
2018-08-17 14:13:02 +0900

    MainActivity singleton instance

--- MusicInstrumentLessoner/app/src/main/java/com/example/jinzzam/musicinstrumentlessoner/myactivity/MainActivity.java
+++ MusicInstrumentLessoner/app/src/main/java/com/example/jinzzam/musicinstrumentlessoner/myactivity/MainActivity.java
@@ -8,11 +8,27 @@ import com.example.jinzzam.musicinstrumentlessoner.session.Session;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static MainActivity instance;
     private static Session session;
+
+    private MainActivity() {
+
+    }
+
+    {
+        instance = MainActivity.getInstance();
+        session = Session.getInstance();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static MainActivity getInstance() {
+        if (instance == null)
+            instance = new MainActivity();
+        return instance;
     }
 }
