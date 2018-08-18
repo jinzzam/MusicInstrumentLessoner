b96d898811d9808c6f5da874552d62d93570bebe
jinzzam <firstmind_@naver.com>
2018-08-18 16:21:24 +0900

    add miniServer

---
+++ miniServer/model.dao/miUserDao.js
@@ -0,0 +1,41 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: '13.209.35.249',
+    post: 3306,
+    user: 'root',
+    password: 1357,
+    database: 'midb'
+});
+
+connection.connect();
+
+this.selectAll = (callback) => {
+    var sql = 'select * from mi_user';
+    connection.query(sql, (err, rows, fields) => {
+        if (!err) {
+            callback(rows);
+        }
+    });
+};
+
+this.selectOne = (email, callback) => {
+    var sql = 'select * from mi_user where email=?';
+    connection.query(sql, email, (err, rows, fields) => {
+        if (!err) {
+            callback(rows);
+        }
+    });
+};
+
+this.insert = (email, password, username, callback) => {
+    var sql = 'insert into mi_user (email,password,username) values (?,?,?)';
+    connection.query(sql, [email, password, username], (err, rows, fields) => {
+        if (!err) {
+            callback(rows);
+        } else {
+            console.log('user post');
+            console.log(err);
+        }
+    });
+};
