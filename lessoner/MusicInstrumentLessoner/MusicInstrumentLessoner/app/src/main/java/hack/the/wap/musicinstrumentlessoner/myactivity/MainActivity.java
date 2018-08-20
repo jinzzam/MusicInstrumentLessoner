6ff1916f2ccd2aee2455a8efa8beed9ab58bff30
jinzzam <firstmind_@naver.com>
2018-08-20 16:44:15 +0900

    Annotate unused threads

--- lessoner/MusicInstrumentLessoner/MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/myactivity/MainActivity.java
+++ lessoner/MusicInstrumentLessoner/MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/myactivity/MainActivity.java
@@ -91,18 +91,18 @@ public class MainActivity extends AppCompatActivity
         MyNavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        new Thread(() -> {
-            while (ivUserMain == null) {
-                ivUserMain = findViewById(R.id.mainUser);
-                Log.e("TAG", "onCreate in Thread >>> " + ivUserMain);
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-        }).start();
+//        new Thread(() -> {
+//            while (ivUserMain == null) {
+//                ivUserMain = findViewById(R.id.mainUser);
+//                Log.e("TAG", "onCreate in Thread >>> " + ivUserMain);
+//                try {
+//                    Thread.sleep(500);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//        }).start();
 
         FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
