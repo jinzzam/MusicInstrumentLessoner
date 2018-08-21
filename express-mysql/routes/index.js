49e3667bd966e029ce2a0c16fd21b89186ce8797
DESKTOP-PLJM5AE\user <vhzkfl2451@naver.com>
2018-08-21 20:33:09 +0900

    android & express-mysql commit

--- express-mysql/routes/index.js
+++ express-mysql/routes/index.js
@@ -2,27 +2,21 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var miUser = require('../model/dao/miUserDao');
-var async = require('async');
 
-app.get('/users', (req, res) => {
+app.post('/users', (req, res) => {
     console.log('who get in here/users');
-    var data;
-    const task1 = function (callback) {
-        miUser.selectAll(function (rows) {
-            data = rows;
-            callback(null);
+    var inputData;
+
+    req.on('data', (data) => {
+        inputData = JSON.parse(data);
+    });
+
+    req.on('end', () => {
+        miUser.selectOne(inputData.searchEmail, function(rows){
+            res.json(rows);
         });
-    };
-    const task2 = function (callback) {
-        console.log(data);
-        callback(null);
-    };
-    const task3 = function (callback) {
-        res.json(data);
-        callback(null);
-    };
-    const tasks = [task1, task2, task3];
-    async.series(tasks);
+    });
+
 });
 
 app.post('/post', (req, res) => {
@@ -34,19 +28,16 @@ app.post('/post', (req, res) => {
     });
 
     req.on('end', () => {
-        console.log("useremail : "+ inputData.useremail + " , name : "+inputData.username + ", password : "+ inputData.password);
-       //miUser.insert(inputData.useremail, inputData.password, inputData.username,function (rows) {
-        //});
-        miUser.selectOne(inputData.useremail, function(rows){
-          console.log(rows);
+        console.log("useremail : "+ inputData.userEmail + " , name : "+inputData.userName + ", password : "+ inputData.userPassword);
+       miUser.insert(inputData.userEmail, inputData.userPassword, inputData.userName,function (rows) {
         });
     });
     res.write('wow');
     res.end();
 });
 
-// app.listen(3100, () => {
-//     console.log('Example app listening on port 3000!');
-// });
+app.listen(3100, () => {
+    console.log('Example app listening on port 3100!');
+});
 
 module.exports = router;
