cda1710775bef1fb10071516280f6aceefab7463
jinzzam <firstmind_@naver.com>
2018-08-22 17:26:54 +0900

    show incorrect segment

--- lessoner/MusicInstrumentLessoner/MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/session/PresentFile.java
+++ lessoner/MusicInstrumentLessoner/MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/session/PresentFile.java
@@ -1,5 +1,5 @@
 package hack.the.wap.musicinstrumentlessoner.session;
 
 public class PresentFile {
-    public static String fileName;
+    public static String fileName = "/storage/self/primary/Music/놀람 교향곡/T/recorded_audio.mp3";
 }
