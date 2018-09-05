ec9caf0a8f434fae1056aab27c7256785b2752da
jinzzam <firstmind_@naver.com>
2018-09-05 20:57:08 +0900

    edit table columns
    # my.sql

--- docs/my.sql
+++ docs/my.sql
@@ -24,10 +24,10 @@ create table music_template_guide(
 );
 
 create table music_template_practice(
+  music_template_practice_id int primary key auto_increment,
+  music_template_id int,
   student_email varchar(30),
   inner_filename varchar(255),
-  music_template_id int,
-  music_template_practice_id int primary key auto_increment,
   constraint fk_musictemplatepractice_miuser_email foreign key (student_email) references mi_user (email) on delete cascade,
   constraint fk_musictemplatepractice_mifile_innerfilename foreign key (inner_filename) references mi_file (inner_filename) on delete cascade,
   constraint fk_musictemplatepractice_musictemplate_musictemplateid foreign key (music_template_id) references music_template (music_template_id) on delete cascade
@@ -36,7 +36,7 @@ create table music_template_practice(
 create table mi_notification(
   mi_notification_id int primary key,
   music_template_id int,
-  regist_date_time datetime default current_timestamp,
+  regist_date_time timestamp,
   type varchar(30) not null,
   comment text(255) not null,
   constraint fk_minotification_musictemplate_musictemplateid foreign key (mi_notification_id) references music_template (music_template_id) on delete cascade
@@ -50,8 +50,9 @@ create table music_template_assignment(
 
 create table music_template_wrong(
   assignment_inner_filename varchar(255),
-  play_time time,
-  constraint pk_musicteplatewrong primary key (assignment_inner_filename, play_time)
+  wrong_time_start time,
+  wrong_time_end time,
+  constraint pk_musicteplatewrong primary key (assignment_inner_filename, wrong_time_start, wrong_time_end)
 );
 
 create table mi_file(
