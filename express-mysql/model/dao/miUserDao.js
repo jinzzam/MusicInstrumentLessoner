d78f7fc0d95f733e90b6bdbbd771fb10f04098aa
jinzzam <firstmind_@naver.com>
2018-09-07 19:15:43 +0900

    mi_user join into music_template
    # miUserDao.js
    # miUser.js

--- express-mysql/model/dao/miUserDao.js
+++ express-mysql/model/dao/miUserDao.js
@@ -1,51 +1,49 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    user : 'root',
-    password : '123',
-    database : 'midb'
+    user: 'root',
+    password: '123',
+    database: 'midb'
 });
 
 connection.connect();
 
-this.selectAll=(callback)=>{
+this.selectAll = (callback) => {
     var sql = 'select * from mi_user';
-    connection.query(sql,function (err, rows, fields) {
-        if(!err){
+    connection.query(sql, function (err, rows, fields) {
+        if (!err) {
             callback(rows);
         }
     });
 };
 
-this.selectOne=(email, callback)=>{
+this.selectOne = (email, callback) => {
     var sql = 'select * from mi_user where email =?';
-    connection.query(sql,[email],function (err, rows, fields) {
-        if(!err){
+    connection.query(sql, [email], function (err, rows, fields) {
+        if (!err) {
             callback(rows);
         }
     });
 };
 
-this.insert =(email, password, username, callback)=>{
+this.insert = (email, password, username, callback) => {
     var sql = 'insert into mi_user values (?,?,?)';
-    connection.query(sql, [email,password,username], (err,rows,fields)=>{
-        if(!err){
+    connection.query(sql, [email, password, username], (err, rows, fields) => {
+        if (!err) {
             callback(rows);
-        } else{
+        } else {
             console.log('user post');
             console.log(err);
         }
     });
 };
 
-this.join =(callback)=>{
-    var sql = 'select mi_user.email, mi_user.username, music_template.music_title FROM mi_user LEFT JOIN music_template ON mi_user.email = music_template.owner = ?';
-    connection.query(sql, (err, result)=>{
-        if(!err){
+this.join = (email, callback) => {
+    var sql = 'select music_template_id, owner, music_title, musician, guide from mi_user right join music_template on mi_user.email = music_template.owner where mi_user.email= ?';
+    connection.query(sql, [email], function (err, rows, fields) {
+        if (!err) {
             callback(rows);
-        } else {
-
         }
-    })
+    });
 };
 
--- express-mysql/routes/api/miUser.js
+++ express-mysql/routes/api/miUser.js
@@ -45,6 +45,27 @@ router.get('/:email', function (req, res, next) {
     async.series(tasks);
 });
 
+router.get('/:email/join', function (req, res, next) {
+    var userEmail = req.params['email'];
+    var data;
+    const task1 = function (callback) {
+        miUser.join(userEmail, function (rows) {
+            data = rows;
+            callback(null);
+        });
+    };
+    const task2 = function (callback) {
+        console.log(data);
+        callback(null);
+    };
+    const task3 = function (callback) {
+        res.send(data);
+        callback(null);
+    };
+    const tasks = [task1, task2, task3];
+    async.series(tasks);
+});
+
 router.get('/:email/:password/:username', function (req, res, next) {
     var userEmail = req.params['email'];
     var userPassword = req.params['password'];
@@ -59,39 +80,5 @@ router.get('/:email/:password/:username', function (req, res, next) {
     async.series(tasks);
 });
 
-router.get('/join', function (req, res, next) {
-    miUser.query({
-        sql: "SELECT \
-              mi_user.email, \
-              mi_user.username, \
-              music_template.music_title, \
-              FROM \
-              mi_user \
-              LEFT JOIN music_template ON mi_user.email = music_template.owner \
-              ",
-        nestTables: '_',
-        values: [id]
-    }, function (err, rows) {
-        var result = [], index = {};
-
-        if (err) throw err;
-
-        rows.forEach(function (row) {
-            if (!(row.users_id in index)) {
-                index[row.users_id] = {
-                    users_id: row.users_id,
-                    users_firstname: row.users_firstname,
-                    rides: []
-                };
-                result.push(index[row.users_id]);
-            }
-            index[row.users_id].rides.push({
-                rides_latitude: row.rides_latitude,
-                rides_longitude: row.rides_longitude
-            });
-        });
-        console.log(result);
-    });
-});
 
 module.exports = router;
