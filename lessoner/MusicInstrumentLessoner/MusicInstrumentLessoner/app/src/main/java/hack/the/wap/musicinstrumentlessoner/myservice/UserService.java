47298ebae50a294151c2262afd70b585e3b8c81f
jinzzam <firstmind_@naver.com>
2018-09-16 19:15:05 +0900

    change file name to UserService
    # UserService.java

---
+++ lessoner/MusicInstrumentLessoner/MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/myservice/UserService.java
@@ -0,0 +1,71 @@
+package hack.the.wap.musicinstrumentlessoner.myservice;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import hack.the.wap.musicinstrumentlessoner.model.dto.MiUserDto;
+import hack.the.wap.musicinstrumentlessoner.session.Session;
+
+public class UserService {
+    private static final String TAG = "USER_SERVICE";
+    RequestQueue queue;
+    private static UserService instance;
+    private static Session session;
+    private static String getUserUrl = "http://192.168.43.36:3000/api/user/";
+    private MiUserDto userDto;
+
+    private UserService(Context context) {
+        queue = Volley.newRequestQueue(context);
+    }
+
+    public static UserService getInstance(Context context) {
+        if (instance == null) {
+            instance = new UserService(context);
+        }
+        return instance;
+    }
+
+
+    public MiUserDto getUserName(String inputEmail) {
+        Log.e(TAG, getUserUrl);
+        final JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, getUserUrl + inputEmail, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    JSONObject user = response.getJSONObject(0);
+                    String userName = user.get("username").toString();
+                    String userEmail = user.get("email").toString();
+                    String userPassword = user.get("password").toString();
+                    userDto = new MiUserDto(userName, userEmail, userPassword);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", "getUserName >>>> : " + error);
+            }
+        });
+        jsonArrayRequest.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+        ));
+        jsonArrayRequest.setTag(TAG);
+        queue.add(jsonArrayRequest);
+        Log.e(TAG, "mainUserVolleySet >>>> : ");
+        return userDto;
+    }
+}
