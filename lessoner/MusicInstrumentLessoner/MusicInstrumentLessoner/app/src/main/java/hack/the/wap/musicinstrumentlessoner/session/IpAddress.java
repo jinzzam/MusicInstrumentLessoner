8229bbed9c973248ddca365874046465b034fac8
jinzzam <firstmind_@naver.com>
2018-10-19 17:10:22 +0900

    change ip address

--- lessoner/MusicInstrumentLessoner/MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/session/IpAddress.java
+++ lessoner/MusicInstrumentLessoner/MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/session/IpAddress.java
@@ -1,7 +1,7 @@
 package hack.the.wap.musicinstrumentlessoner.session;
 
 public class IpAddress {
-    String ip = "175.199.255.108";
+    String ip = "172.30.66.122";
 
     public String getIp() {
         return ip;
