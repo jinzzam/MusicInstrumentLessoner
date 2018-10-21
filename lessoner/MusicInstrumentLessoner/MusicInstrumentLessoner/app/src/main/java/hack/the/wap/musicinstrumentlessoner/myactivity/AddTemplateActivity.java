3d734b31a5510bd843efe4ef90d6d6d439163da6
jinzzam <firstmind_@naver.com>
2018-10-22 06:34:51 +0900

    마지막 버그 수정 및 어플 색상 수정

--- lessoner/MusicInstrumentLessoner/MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/myactivity/AddTemplateActivity.java
+++ lessoner/MusicInstrumentLessoner/MusicInstrumentLessoner/app/src/main/java/hack/the/wap/musicinstrumentlessoner/myactivity/AddTemplateActivity.java
@@ -5,15 +5,21 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import hack.the.wap.musicinstrumentlessoner.R;
+import hack.the.wap.musicinstrumentlessoner.debug.DebugImageMatch;
+import hack.the.wap.musicinstrumentlessoner.session.Session;
 
 public class AddTemplateActivity extends AppCompatActivity {
+    private Session session = Session.getInstance();
     private ImageView ivAddTemplateActLeftArrow;
+    private ImageView ivAddTemplateActUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_template);
         ivAddTemplateActLeftArrow = findViewById(R.id.ivAddTemplateActLeftArrow);
+        ivAddTemplateActUser = findViewById(R.id.ivAddTemplateActUser);
+        ivAddTemplateActUser.setImageResource(DebugImageMatch.getImageFromName(session.getMainUser().getName()));
         viewSetListener();
     }
 
